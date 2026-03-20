47a3d00e3719a74dc207d7af27390d063da4e0d3
avipednekar <avinashpednekar431@gmail.com>
2026-03-20 12:40:30 +0530

    feat: update resume data and enhance MongoDB connection settings

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}/ai_recruitment`);
     console.log(`✅ MongoDB Connected (Mongoose): ${conn.connection.host}`);
   } catch (error) {
-    console.error(`❌ Error connecting to MongoDB: ${error.message}`);
+    console.error(`❌ Error connecting to MongoDB: ${error}`);
     process.exit(1);
   }
 };
