caa723013a0ebd48cd75b0d2c059266e5606a534
avipednekar <avinashpednekar431@gmail.com>
2026-04-19 15:15:11 +0530

    chore: untrack .planning/, test infra, and GEMINI.md from git

--- backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  testEnvironment: "node",
-  transform: {},
-  extensionsToTreatAsEsm: [],
-  testMatch: ["**/__tests__/**/*.test.js"],
-  testTimeout: 30000,
-  verbose: true,
-};
